bb262c0264ca0d877e74c1b57f54a1ec3402e45f
Haprishwa <144096278+Haprishwa@users.noreply.github.com>
2024-08-25 12:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app
     });
   });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
+
